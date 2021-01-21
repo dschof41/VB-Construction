da7a29129ac9d0b1ddbf8bf4154bcd17701d6f66
dschof41 <dschof41@gmail.com>
2021-01-20 20:02:20 -0500

    Update project naming
    update naming to vb construction

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'VB Conscruction Business Site',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Business Site for VB Construction LLC',
   },
   plugins: [
     'gatsby-plugin-react-helmet',

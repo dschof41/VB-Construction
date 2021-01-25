da762eab0f576f83fd9b8f80d3ea25c846448f60
dschof41 <dschof41@gmail.com>
2021-01-25 14:49:48 -0500

    add bot-field
    add bot-field

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -22,6 +22,7 @@ export default class Index extends React.Component {
                 name="contact"
                 method="POST"
                 data-netlify="true"
+                data-netlify-honeypot="bot-field"
               >
                 {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
                 <input type="hidden" name="form-name" value="contact" />
